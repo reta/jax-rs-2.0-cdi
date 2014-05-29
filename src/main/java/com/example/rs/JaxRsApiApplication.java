c589db2254bf665acc851f79a2e5aaca6e796ef3
reta <drreta@gmail.com>
2014-05-28 21:58:28 -0400

    Added customizable application sample

--- src/main/java/com/example/rs/JaxRsApiApplication.java
+++ src/main/java/com/example/rs/JaxRsApiApplication.java
@@ -1,8 +1,28 @@
 package com.example.rs;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
 @ApplicationPath( "api" )
 public class JaxRsApiApplication extends Application {
+	@Inject private PeopleRestService peopleRestService;
+	@Produces private JacksonJsonProvider jacksonJsonProvider = new JacksonJsonProvider();  
+	
+	@Override
+	public Set< Object > getSingletons() {
+		return new HashSet<>(
+			Arrays.asList( 
+		        peopleRestService, 
+		        jacksonJsonProvider 
+		    )
+	    );
+	}
 }
